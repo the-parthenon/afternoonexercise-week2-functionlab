6d1fccaeffa5b1f702bc2aa7c6a3921fdd3206df
the-parthenon <ericinportland@gmail.com>
2022-03-28 16:51:01 -0700

    Solutions through Problem 22 implemented.

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -354,15 +354,29 @@ const upperString = (str) => {
   You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
 */
 
-//CODE HERE
+//See answer to problem 19 (Did the Math.floor thing in the initial question before reading this one.)
 
 ////////////////// PROBLEM 21 ////////////////////
-let sampleArray = [0, 1, 2, 3, 4, 7, 5, 6, 8, 9];
+let sampleArray = [0, 1, 2, 3, 4, 5, 6, 8, 7];
 /*
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
 
-//CODE HERE
+function ascendCheck(arr) {
+  let smallerCheck = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= smallerCheck) {
+      return false;
+    } else {
+      smallerCheck = arr[i];
+      // console.log(smallerCheck);
+    }
+  }
+  return true;
+}
+
+const arrayIsAscending = ascendCheck(sampleArray);
+console.log(arrayIsAscending);
 
 ////////////////// PROBLEM 22 ////////////////////
 
@@ -386,13 +400,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = [];
+let globalScope = ['cute'];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = [];
+let bathroomScope = ['cute, squeaky, nautical'];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = [];
+let bathtubScope = ['cute, nautical'];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = [];
+let pondScope = ['cute, fluffy'];
