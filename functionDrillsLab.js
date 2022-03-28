f8e3b32eb3428f5a7f366d0cba58ce8fe3fcb693
the-parthenon <ericinportland@gmail.com>
2022-03-28 16:59:11 -0700

    Refactored Problem 5 to use a ternary statement. Final commit.

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -77,6 +77,9 @@ const greeting = function (name) {
 // };
 // console.log(compareNums(4, 6));
 
+const compareNums = (num1, num2) => (num1 > num2 ? num1 : num2);
+console.log(compareNums(-19, -5));
+
 ////////////////// PROBLEM 6 ////////////////////
 /*
   Create a function called add that takes in two parameters 
@@ -362,21 +365,21 @@ let sampleArray = [0, 1, 2, 3, 4, 5, 6, 8, 7];
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
 
-function ascendCheck(arr) {
-  let smallerCheck = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] <= smallerCheck) {
-      return false;
-    } else {
-      smallerCheck = arr[i];
-      // console.log(smallerCheck);
-    }
-  }
-  return true;
-}
+// function ascendCheck(arr) {
+//   let smallerCheck = arr[0];
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] <= smallerCheck) {
+//       return false;
+//     } else {
+//       smallerCheck = arr[i];
+//       // console.log(smallerCheck);
+//     }
+//   }
+//   return true;
+// }
 
-const arrayIsAscending = ascendCheck(sampleArray);
-console.log(arrayIsAscending);
+// const arrayIsAscending = ascendCheck(sampleArray);
+// console.log(arrayIsAscending);
 
 ////////////////// PROBLEM 22 ////////////////////
 
